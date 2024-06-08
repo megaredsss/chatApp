56da2c213c0e933267bb921bed6d8aca0ff6dfcb
megaredsss <58664975+megaredsss@users.noreply.github.com>
2024-06-08 13:46:46 +0300

    add routes

--- pkg/websocketCreater.go
+++ pkg/websocketCreater.go
@@ -14,6 +14,13 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+	http.HandleFunc("/ws", wsCreater)
+}
+
 func wsCreater(response http.ResponseWriter, request *http.Request) {
 	conn, err := (&websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}).Upgrade(response, request, nil)
 	if err != nil {
