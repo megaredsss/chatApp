96503039b0010d3c7690d1d823a602c7190e5ac8
megaredsss <58664975+megaredsss@users.noreply.github.com>
2024-06-08 13:45:56 +0300

    another version of upgrader

--- pkg/websocketCreater.go
+++ pkg/websocketCreater.go
@@ -7,6 +7,13 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
 func wsCreater(response http.ResponseWriter, request *http.Request) {
 	conn, err := (&websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}).Upgrade(response, request, nil)
 	if err != nil {
