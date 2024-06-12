0448906106c4adbe3634404e03bd3ff0722c15fc
megaredsss <58664975+megaredsss@users.noreply.github.com>
2024-06-12 22:11:03 +0300

    Add router and comments

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+// default page
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+}
 func main() {
+	setupRoutes()
 	fmt.Println("Hello world")
 }
--- pkg/models/client.go
+++ pkg/models/client.go
@@ -2,6 +2,7 @@ package pkg
 
 import "github.com/gorilla/websocket"
 
+// Client need to add
 type Client struct {
 	ws     *websocket.Conn
 	ch     chan *Message
