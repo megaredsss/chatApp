cf077b2da6816d7de03b19722340e7ac4aa9ebbc
megaredsss <58664975+megaredsss@users.noreply.github.com>
2024-09-23 10:46:43 +0300

    add connect to postgres

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,16 +2,46 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-// default page
-func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Simple Server")
-	})
+type User struct {
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
 }
+
 func main() {
-	setupRoutes()
-	fmt.Println("Hello world")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
+	dbName := os.Getenv("DB_DATABASE")
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	db, err := gorm.Open(postgres.Open(fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUsername, dbPassword, dbName)), &gorm.Config{})
+	if err != nil {
+		fmt.Printf("Error connecting to database: %s", err)
+	}
+
+	err = db.AutoMigrate(&User{})
+	if err != nil {
+		fmt.Println("Error during migration")
+	}
+	fmt.Println(db)
+	//  claim := JWT.Claims{
+	//    Role: "Boss",
+	//  }
+	//  JWT.GetToken()
+	//  fmt.Println(claim)
 }
